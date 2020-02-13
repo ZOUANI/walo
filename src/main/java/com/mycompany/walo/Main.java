3f3554026d827aa06c93639e45a210a389e613be
MoulaYounes <MoulaYounes@DESKTOP-GBNILO9>
2020-02-13 20:44:02 +0100

    fix path

---
+++ src/main/java/com/mycompany/walo/Main.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.walo;
+
+/**
+ *
+ * @author MoulaYounes
+ */
+public class Main {
+    public static void main(String[] args) {
+//        sh 'rm -rf mavenproject2'
+        System.out.println("kokooooo zouani");
+    }
+}
